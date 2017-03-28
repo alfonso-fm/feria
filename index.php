438c2f1a5483f615a268eca62085b86e660024c9
Flores Martinez <alfonso.flores@abbott.com>
2017-03-28 10:18:37 -0600

    comentario

---
+++ index.php
@@ -0,0 +1,15 @@
+<h1>EJEMPLO DE GIT + GITHUB + HEROKU         </h1><br /><hr>
+<h2>GIT EN MI MAQUINA                        </h2><br />
+<h3>GITHUB EN MI CUENTA alfonso-fm/feria_code</h3><br />
+<h4>HEROKU EN MI CUENTA heroku/ferianueva    </h4><br /><hr>
+
+<?php
+
+// Muestra toda la información, por defecto INFO_ALL
+phpinfo();
+
+// Muestra solamente la información de los módulos.
+// phpinfo(8) hace exactamente lo mismo.
+phpinfo(INFO_MODULES);
+
+?>
